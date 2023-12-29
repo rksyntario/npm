c0fb3863d340006525e1ce2a67a9c5d07fa2862f
rksyntario <rksyntario@gmail.com>
2023-12-29 15:48:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+ function App() {
+  return (
+    <h1 className="text-xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+export default App;
